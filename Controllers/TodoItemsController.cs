f2f4d145811e7c6c6333f31e32e5a2ed71639be2
cartel360 <billycartel360@gmail.com>
2023-06-11 21:09:22 +0300

    project setup

---
+++ Controllers/TodoItemsController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestHttp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TodoItemsController : ControllerBase
+    {
+        private static List<TodoItem> _todoItems = new List<TodoItem>();
+        private static int _nextId = 1;
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return _todoItems;
+        }
+
+        // GET: api/TodoItems/{id}
+        [HttpGet("{id}")]
+        public ActionResult<TodoItem> Get(int id)
+        {
+            var todoItem = _todoItems.FirstOrDefault(item => item.Id == id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            return todoItem;
+        }
+
+        // POST: api/TodoItems
+        [HttpPost]
+        public ActionResult<TodoItem> Post(TodoItem todoItem)
+        {
+            todoItem.Id = _nextId++;
+            _todoItems.Add(todoItem);
+
+            return CreatedAtAction(nameof(Get), new { id = todoItem.Id }, todoItem);
+        }
+
+        // PUT: api/TodoItems/{id}
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, TodoItem updatedTodoItem)
+        {
+            var existingTodoItem = _todoItems.FirstOrDefault(item => item.Id == id);
+            if (existingTodoItem == null)
+            {
+                return NotFound();
+            }
+
+            existingTodoItem.Title = updatedTodoItem.Title;
+            existingTodoItem.IsCompleted = updatedTodoItem.IsCompleted;
+
+            return NoContent();
+        }
+
+        // DELETE: api/TodoItems/{id}
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var todoItem = _todoItems.FirstOrDefault(item => item.Id == id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _todoItems.Remove(todoItem);
+
+            return NoContent();
+        }
+    }
+
+}
---
+++ TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TestHttp
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+
+}
